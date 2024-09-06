8920a068a2a8d972c64a1d41eccd5513cc207d98
Aggre <hiroyuki.aggre@gmail.com>
2024-09-06 15:24:15 +0900

    add some test scenarios

---
+++ contracts/mocks/ERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity =0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IWithdraw.sol";
+
+contract MockToken is ERC20 {
+	address public _withdraw;
+
+	constructor(uint256 initialSupply, address withdraw) ERC20("Mock", "MOC") {
+		_mint(msg.sender, initialSupply);
+		_withdraw = withdraw;
+	}
+
+	function transfer(
+		address _to,
+		uint256 _value
+	) public override returns (bool) {
+		IWithdraw(_withdraw).beforeBalanceChange(msg.sender, _to);
+
+		/**
+		 * Calls the transfer of ERC20.
+		 */
+		_transfer(msg.sender, _to, _value);
+		return true;
+	}
+}
--- test/example.ts
+++ test/example.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { ethers, upgrades } from 'hardhat'
 
-describe('Example', () => {
+describe('ClubsVault', () => {
 	describe('initialize', () => {
 		describe('success', () => {
 			it('initialize value', async () => {
@@ -26,14 +26,206 @@ describe('Example', () => {
 		})
 	})
 
-	describe('add', () => {
-		it('Add the passed value to `value`', async () => {
-			const Example = await ethers.getContractFactory('Example')
-			const example = await upgrades.deployProxy(Example, [1n])
-			await example.add(10n)
-			const value: bigint = await example.value()
+	describe('Scenarios', () => {
+		describe('MOST SIMPLE: Every token balance will not change once received', () => {
+			describe('Cycle 1: Vault has 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					it('Alice has 20%, and can withdraw 20 ETH.')
 
-			expect(value).to.equal(11n)
+					it('Alice can not withdraw ETH now.')
+
+					it('Bob has 30%, and can withdraw 30 ETH.')
+
+					it('Bob can not withdraw ETH now.')
+
+					it('Carol has 50%, and can withdraw 50 ETH.')
+
+					it('Carol can not withdraw ETH now.')
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					it('Alice has 20%, and can withdraw 20 tokens.')
+
+					it('Alice can not withdraw tokens now.')
+
+					it('Bob has 30%, and can withdraw 30 tokens.')
+
+					it('Bob can not withdraw tokens now.')
+
+					it('Carol has 50%, and can withdraw 50 tokens.')
+
+					it('Carol can not withdraw tokens now.')
+				})
+			})
+			describe('Cycle 2: Vault reacquired 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					it('Alice has 20%, and can withdraw 20 ETH.')
+
+					it('Alice can not withdraw ETH now.')
+
+					it('Bob has 30%, and can withdraw 30 ETH.')
+
+					it('Bob can not withdraw ETH now.')
+
+					it('Carol has 50%, and can withdraw 50 ETH.')
+
+					it('Carol can not withdraw ETH now.')
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					it('Alice has 20%, and can withdraw 20 tokens.')
+
+					it('Alice can not withdraw tokens now.')
+
+					it('Bob has 30%, and can withdraw 30 tokens.')
+
+					it('Bob can not withdraw tokens now.')
+
+					it('Carol has 50%, and can withdraw 50 tokens.')
+
+					it('Carol can not withdraw tokens now.')
+				})
+			})
+		})
+
+		describe('Transfer the tokens to someone else after withdrawing', () => {
+			describe('Cycle 1: Vault has 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					describe('Alice has 20%, and will send 10% to Bob after withdrawing. Bob will have 40%.', () => {
+						it(
+							'Alice has 20%, and can withdraw 20 ETH. Then, send 10% of tokens to Bob.',
+						)
+
+						it('Alice can not withdraw ETH now.')
+
+						it('Bob has 40%, and can withdraw **30** ETH.')
+
+						it('Bob can not withdraw ETH now.')
+
+						it('Carol has 50%, and can withdraw 50 ETH.')
+
+						it('Carol can not withdraw ETH now.')
+					})
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					describe('Alice has 20%, and will send 10% to Bob after withdrawing. Bob will have 40%.', () => {
+						it(
+							'Alice has 20%, and can withdraw 20 tokens. Then, send 10% of tokens to Bob.',
+						)
+
+						it('Alice can not withdraw tokens now.')
+
+						it('Bob has 40%, and can withdraw **30** tokens.')
+
+						it('Bob can not withdraw tokens now.')
+
+						it('Carol has 50%, and can withdraw 50 tokens.')
+
+						it('Carol can not withdraw tokens now.')
+					})
+				})
+			})
+			describe('Cycle 2: Vault reacquired 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					it('Alice has 10%, and can withdraw 10 ETH.')
+
+					it('Alice can not withdraw ETH now.')
+
+					it('Bob has 40%, and can withdraw **40** ETH.')
+
+					it('Bob can not withdraw ETH now.')
+
+					it('Carol has 50%, and can withdraw 50 ETH.')
+
+					it('Carol can not withdraw ETH now.')
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					it('Alice has 10%, and can withdraw 10 tokens.')
+
+					it('Alice can not withdraw tokens now.')
+
+					it('Bob has 40%, and can withdraw **40** tokens.')
+
+					it('Bob can not withdraw tokens now.')
+
+					it('Carol has 50%, and can withdraw 50 tokens.')
+
+					it('Carol can not withdraw tokens now.')
+				})
+			})
+		})
+
+		describe('Token balance will decrease once but will increase again', () => {
+			describe('Cycle 1: Vault has 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					describe('Alice has 20%, and will send 10% to Bob after withdrawing. Bob will have 40%. Carol will send 30% to Alice **before** withdrawing.', () => {
+						it(
+							'Alice has 20%, and can withdraw 20 ETH. Then, send 10% of tokens to Bob.',
+						)
+
+						it('Alice can not withdraw ETH now.')
+
+						it('Bob has 40%, and can withdraw **30** ETH.')
+
+						it('Bob can not withdraw ETH now.')
+
+						it('Carol has 50%, and send 30% of tokens to Alice')
+
+						it('Carol has 20% now, and can withdraw **20** ETH.')
+
+						it('Carol can not withdraw ETH now.')
+
+						it('Alice has 40%, and can withdraw **30** ETH now.')
+					})
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					describe('Alice has 20%, and will send 10% to Bob after withdrawing. Bob will have 40%. Carol will send 30% to Alice **before** withdrawing.', () => {
+						it(
+							'Alice has 20%, and can withdraw 20 tokens. Then, send 10% of tokens to Bob.',
+						)
+
+						it('Alice can not withdraw tokens now.')
+
+						it('Bob has 40%, and can withdraw **30** tokens.')
+
+						it('Bob can not withdraw tokens now.')
+
+						it('Carol has 50%, and send 30% of tokens to Alice')
+
+						it('Carol has 20% now, and can withdraw **20** tokens.')
+
+						it('Carol can not withdraw tokens now.')
+
+						it('Alice has 40%, and can withdraw **30** tokens now.')
+					})
+				})
+			})
+			describe('Cycle 2: Vault reacquired 100 ETH, 100 tokens.', () => {
+				describe('Vault has 100 ETH(native token)', () => {
+					it('Alice has 40%, and can withdraw 40 ETH.')
+
+					it('Alice can not withdraw ETH now.')
+
+					it('Bob has 40%, and can withdraw **40** ETH.')
+
+					it('Bob can not withdraw ETH now.')
+
+					it('Carol has 20%, and can withdraw 20 ETH.')
+
+					it('Carol can not withdraw ETH now.')
+				})
+				describe('Vault has 100 tokens(ERC-20)', () => {
+					it('Alice has 40%, and can withdraw 40 tokens.')
+
+					it('Alice can not withdraw tokens now.')
+
+					it('Bob has 40%, and can withdraw **40** tokens.')
+
+					it('Bob can not withdraw tokens now.')
+
+					it('Carol has 50%, and can withdraw 50 tokens.')
+
+					it('Carol can not withdraw tokens now.')
+				})
+			})
 		})
 	})
 })
